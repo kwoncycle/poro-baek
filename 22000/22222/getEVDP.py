49a85e2e443ae0a2e2d8fea1bb587bd2cce805b5
kwoncycle <kwoncycle@postech.ac.kr>
2025-01-31 23:28:26 +0900

    increase effiency

--- 22000/22222/getEVDP.py
+++ 22000/22222/getEVDP.py
@@ -1,11 +1,10 @@
 from decimal import *
 from tqdm import tqdm
 
-getcontext().prec = 500
-
 with open("MAXN", "r") as f:
     MAX_N = int(f.readline())
 
+getcontext().prec = MAX_N * 10
 
 Cb = [[0 for _ in range(MAX_N + 1)] for _ in range(MAX_N + 1)]
 
@@ -17,29 +16,21 @@ for i in range(MAX_N + 1):
 
 
 EV = [[Decimal("0") for _ in range(MAX_N + 1)] for _ in range(MAX_N + 1)]
-DP = [[Decimal("0") for _ in range(MAX_N + 1)] for _ in range(MAX_N + 1)]
-# DP[p][q] = sum_{i=0}^{q} qCi E[D^p D*^i]
 
 EV[0][0] = Decimal("1.0")
 
 print("[+] Calculating EVs...")
-for p in tqdm(range(MAX_N + 1)):
+for p in tqdm(range(1, MAX_N + 1)):
     for q in range(MAX_N + 1):
         if (p - q) % 6 == 0:
             # target EV[p][q], EV[q][p]
-            if p+q > 0:
-                for i in range(p + 1):
-                    if i < p:
-                        EV[p][q] = EV[p][q] + (Cb[p][i] * DP[i][q])
+            for i in range(p + 1):
+                for j in range(q + 1):
+                    if i == p and q == j:
+                        continue
                     else:
-                        for j in range(q + 1):
-                            if i == p and j == q: # d_{p,q}(i, j)
-                                continue
-                            if (i-j) % 6 == 0:
-                                EV[p][q] = EV[p][q] + (Cb[p][i] * Cb[q][j]) * EV[i][j]
-                EV[p][q] = EV[p][q] * Decimal(2) / Decimal(3 * (pow(2, p + q) - 1))
-
-        for i in range(q + 1):
-            DP[p][q] = DP[p][q] + Cb[q][i] * EV[p][i]
-
+                        if (i-j) % 6 == 0:
+                            EV[p][q] = EV[p][q] + (Cb[p][i] * Cb[q][j]) * EV[i][j]
+            EV[p][q] = EV[p][q] * Decimal(2) / Decimal(3 * (pow(2, p + q) - 1))
+            EV[q][p] = EV[p][q]
 print("[+] Done!")
